fef146884bc8e667801709970c1b2955812c0dfc
schcirrus <mciver.dave@gmail.com>
2018-11-02 17:09:33 +1300

    Update loofah gem re. security alert from GitHub

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,9 +1,11 @@
-class Payment < ApplicationRecord
+class Payment < ActiveRecord::Base
+
   attr_accessor :card_number, :card_cvv, :card_expires_month, :card_expires_year
+
   belongs_to :user
 
   def self.month_options
-    Date::MONTHNAMES.compact.each_with_index.map {|name, i|["#{i+1} - #{name}", i+1] }
+    Date::MONTHNAMES.compact.each_with_index.map { |name, i| ["#{i+1} - #{name}", i+1]}
   end
 
   def self.year_options
@@ -12,7 +14,6 @@ class Payment < ApplicationRecord
 
   def process_payment
     customer = Stripe::Customer.create email: email, card: token
-
     Stripe::Charge.create customer: customer.id,
                           amount: 1000,
                           description: 'Premium',

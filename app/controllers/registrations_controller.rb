4f4cef24310785245bed3e88e6d77c20ebc2661d
schcirrus <mciver.dave@gmail.com>
2018-11-06 09:25:35 +1300

    Add stripe payment functionality

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -20,7 +20,7 @@ class RegistrationsController < Devise::RegistrationsController
           render :new and return
         end
 
-        if resource.active_for_authentication
+        if resource.active_for_authentication?
           set_flash_message :notice, :signed_up if is_flashing_format?
           sign_up(resource_name, resource)
           respond_with resource, location: after_sign_up_path_for(resource)
